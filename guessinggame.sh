915f7e0447dcfd2e40efd7306f177b310d3541c5
AbstractMonkey <sean.hobin@gmail.com>
2021-12-22 02:49:44 -0500

    painfully verbose and explicit detail

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,35 @@
-# Assignment 4: Bash, Make, Git, and GitHub
-# Submitted by Sean Hobin
-# Written in 38 lines, or 27 lines of code
-
+#######################################################################################
+# Assignment 4: Bash, Make, Git, and GitHub                                           #
+# Submitted by Sean Hobin                                                             #
+# Run via "bash guessinggame.sh"                                                      #
+#######################################################################################
 # Init variables
 numfiles=$(ls | wc -l)
 re='^[0-9]+$'
 correct=1
-
-# Function component of assignment
+#######################################################################################
+# FUNCTION COMPONENT of assignment                                                    #
+#######################################################################################
 function finishgame {
   let correct=0
 }
-
-echo "How many files are in the current directory?"
+#######################################################################################
+# Ask the user how many files are in the current directory, then prompt them to guess #
+#######################################################################################
+echo "How many files are in the current directory? Give it a guess!: "
 read guess
-
-# Loop component of assignment
+#######################################################################################
+# LOOP COMPONENT of assignment                                                        #
+#######################################################################################
 while [[ $correct -eq 1 ]]
 do
-# If statement components of assignment
+#######################################################################################
+# IF STATEMENT COMPONENTS of assignment                                               #
+# If the user guesses incorrectly, they are advised whether their guess is too high   #
+# or too low, and are prompted to guess again. If user answers correctly, they are    #
+# congratulated. The program does not end until the user enters the correct number of #
+# files. Program is run via entering "bash guessinggame.sh" into the console.         #
+#######################################################################################
   if ! [[ $guess =~ $re ]]
   then
     echo "I can only handle numbers, and simple ones like ints. Try again!: "
